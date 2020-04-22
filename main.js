855bfe8b0dac956f898605a54bfded6e14fe838a
rock112293 <62177410+rock112293@users.noreply.github.com>
2020-04-22 17:54:21 +0800

    Add files via upload
    Feature:
    ●Dropdown Menu
    ●Icons/Logo
    ●Header

---
+++ main.js
@@ -0,0 +1,11 @@
+alert('Warning! this codes are licensed under MIT');
+//Github Project
+//('font-style')fonts.google.com
+//Developed and made in html5,css3,js
+//script for menu toggle with responsive viewport.
+//edit in Acode-powerful code editor(Beta)Android Application version:0.1.10.100 
+//script type="text/javascript"
+function toggle(){
+      var header = document.getElementById("header");
+      header.classList.toggle('active');
+    }
