f6ad7fec2dfe923d1421998cd9a8e328ad0e179e
rock112293 <62177410+rock112293@users.noreply.github.com>
2020-04-26 15:29:51 +0800

    update main.js

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 alert('Warning! this codes are licensed under MIT');
 //Github/rock112293/Repository///////////
 //('font-style')fonts.google.com/////////
-
-//Edit in Acode-powerful code editor Beta Android Application version:0.1.10.100
+///////////Theme:color white/////////////
+//Edit in Acode-powerful code editor Beta Android Application version:0.1.11.103
 
 //////script type="text/javascript"//////
 function toggle(){
       var header = document.getElementById("header");
       header.classList.toggle('active');
     }
+    
